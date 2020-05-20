3a625ab3155eca2cad1ec007ff8a56a641c44662
Niranjan <niranjan.rath@rabobank.nl>
2020-05-20 09:59:07 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {ClientsComponent} from './clients/clients.component';
+import {ProductsComponent} from './products/products.component';
+import {UsersComponent} from './users/users.component';
+import {VisitorsComponent} from './visitors/visitors.component';
+import {AppointmentsComponent} from './appointments/appointments.component';
+import {LoginComponent} from './login/login.component';
+import {PagenotfoundComponent} from './pagenotfound/pagenotfound.component';
 
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'home', component: DashboardComponent },
+  { path: 'clients', component: ClientsComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'visitors', component: VisitorsComponent },
+  { path: 'appointments', component: AppointmentsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: PagenotfoundComponent }
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ProductsComponent } from './products/products.component';
+import { VisitorsComponent } from './visitors/visitors.component';
+import { UsersComponent } from './users/users.component';
+import { LoginComponent } from './login/login.component';
+import { AppointmentsComponent } from './appointments/appointments.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ClientsComponent,
+    ProductsComponent,
+    VisitorsComponent,
+    UsersComponent,
+    LoginComponent,
+    AppointmentsComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
